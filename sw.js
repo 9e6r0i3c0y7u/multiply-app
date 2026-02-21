c5617a5d28532e2c1329c36bb74aac3ea7160bb5
9e6r0i3c0y7u <didiericyu@gmail.com>
2026-02-21 16:37:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "app-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+];
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-   caches.open("app-cache-v2").then(cache =>
-      cache.addAll(["./", "./index.html"])
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k.startsWith("app-cache-") && k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+          return null;
+        })
+      )
     )
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then((r) => r || fetch(e.request))
   );
-
 });
