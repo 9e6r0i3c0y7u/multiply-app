4a450f9fceacf6f62f30cdee61fb41bcc240a72e
9e6r0i3c0y7u <didiericyu@gmail.com>
2026-02-21 17:27:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,13 @@
-const CACHE_NAME = "app-cache-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./sw.js",
-];
-
-self.addEventListener("install", (e) => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((k) => {
-          if (k.startsWith("app-cache-") && k !== CACHE_NAME) {
-            return caches.delete(k);
-          }
-          return null;
-        })
-      )
+    caches.open("app-cache").then(cache =>
+      cache.addAll(["./", "./index.html"])
     )
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
-
-
-
-
